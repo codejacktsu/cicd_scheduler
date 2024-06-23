9402695f55075eae2ffcdb6e98280d964fb2d6dc
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2024-06-22 19:20:11 -0500

    add logging output

--- src/app.py
+++ src/app.py
@@ -9,6 +9,12 @@ logging.basicConfig(level=logging.DEBUG,
 # Create a logger
 logger = logging.getLogger(__name__)
 
+# output file handler
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levellevel)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logger.addHandler(file_handler)
+
 # Sample log messages
 logger.debug('This is a debug message')
 logger.info('This is an info message')
