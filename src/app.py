ad5f9dcb116c76b2cc0ff884d9f84a820bb800a0
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2024-06-22 19:43:12 -0500

    refactor app

--- src/app.py
+++ src/app.py
@@ -1,23 +1,14 @@
-import logging
+from utils.log_manager import create_logger
 
 
-# Define the basic configuration
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+def main():
+    # Sample log messages
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
 
-# Create a logger
-logger = logging.getLogger(__name__)
-
-# output file handler
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
-logger.addHandler(file_handler)
-
-# Sample log messages
-logger.debug('This is a debug message')
-logger.info('This is an info message')
-logger.warning('This is a warning message')
-logger.error('This is an error message')
-logger.critical('This is a critical message')
+if __name__ == "__main__":
+    logger = create_logger()
+    main()
